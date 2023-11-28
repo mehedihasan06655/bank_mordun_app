04136881f343fff5e9505cfccaefc2cd871fc9d1
Mehedi <mh6040500@gmail.com>
2023-11-28 21:56:40 +0600

    cta part dodne

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,8 +1,8 @@
 
 const Button = ({styles}) => {
   return (
-    <button type="button" className={`${styles} py-4 px-6 font-poppins text-[18px] font-medium text-primary bg-blue-gradient`}>
-      GetStarted
+    <button type="button" className={`${styles} py-4 px-6 font-poppins text-[18px] font-medium text-primary bg-blue-gradient rounded-[10px]`}>
+      Get Started
     </button>
   )
 }
